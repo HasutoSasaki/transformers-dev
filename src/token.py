1747f24ab3c097d08154b906cee1a28bad910ec1
HasutoSasaki <hasuto.sa@gmail.com>
2025-08-28 07:22:01 +0900

    feat: add token.py for BERT tokenization

---
+++ src/token.py
@@ -0,0 +1,8 @@
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
+
+sequence = "Using a Transformer network is simple"
+tokens = tokenizer.tokenize(sequence)
+
+print(tokens)
