bb9c369781c13bf7f7d8149593986ba83a8dd12d
HasutoSasaki <hasuto.sa@gmail.com>
2025-08-31 22:03:03 +0900

    feat: add tokenizer_demo.py for demonstrating BERT tokenization

--- src/tokenizer_demo.py
+++ src/tokenizer_demo.py
@@ -6,3 +6,11 @@ sequence = "Using a Transformer network is simple"
 tokens = tokenizer.tokenize(sequence)
 
 print(tokens)
+
+# token IDs
+ids = tokenizer.convert_tokens_to_ids(tokens)
+print(ids)
+
+# decode
+decoded = tokenizer.decode(ids)
+print(decoded)
